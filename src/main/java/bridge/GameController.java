bbc7ec930d527591784659af34b3fde1defb4090
Jihyun3478 <leejihyun3478@gmail.com>
2026-01-01 20:37:27 +0900

    feat: 게임 시작 문구를 출력하는 기능 구현

---
+++ src/main/java/bridge/GameController.java
@@ -0,0 +1,15 @@
+package bridge;
+
+public class GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        outputView.startMessage();
+    }
+}
